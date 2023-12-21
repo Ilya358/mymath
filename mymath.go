2494e3de4d3dbbd139a447363b8c085fd915807a
ilusha <resp16@mail.ru>
2023-12-21 12:33:22 +0300

    init commit

---
+++ mymath.go
@@ -0,0 +1,55 @@
+package mymath
+
+import "math"
+
+func Sqrt(x float64) float64 {
+	return math.Sqrt(x)
+}
+func Ceil(x float64) float64 {
+	return math.Ceil(x)
+}
+func Floor(x float64) float64 {
+	return math.Floor(x)
+}
+func Pow(x float64, y float64) float64 {
+	return math.Pow(x, y)
+}
+func Max(x float64, y float64) float64 {
+	return math.Max(x, y)
+}
+func Min(x float64, y float64) float64 {
+	return math.Min(x, y)
+}
+func Trunc(x float64) float64 {
+	return math.Trunc(x)
+}
+func Round(x float64) float64 {
+	return math.Round(x)
+}
+func RoundToEven(x float64) float64 {
+	return math.RoundToEven(x)
+}
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+func Acosh(x float64) float64 {
+	return math.Acosh(x)
+}
+func Asin(x float64) float64 {
+	return math.Asin(x)
+}
+func Atan(x float64) float64 {
+	return math.Atan(x)
+}
+func Exp(x float64) float64 {
+	return math.Exp(x)
+}
+func Cos(x float64) float64 {
+	return math.Cos(x)
+}
+func Sin(x float64) float64 {
+	return math.Sin(x)
+}
+func Tan(x float64) float64 {
+	return math.Tan(x)
+}
